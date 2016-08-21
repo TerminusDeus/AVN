68425235179c9be9dc15f388e5e50e4dd62a2eb8
Alexander <grindsoul@yandex.ru>
2016-08-21 19:29:37 +0400

    Add project files.

---
+++ WindowsFormsApplication1/Boo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public sealed class Boo
+    {
+        private readonly int _id;
+
+        public Boo()
+        {
+            NotMain();
+        }
+
+        public Boo(int id)
+        {
+            _id = id;
+        }
+
+        public static IEnumerable<Boo> Generate(int numberOfElements)
+        {
+            return Enumerable.Range(1, numberOfElements).Select(id => new Boo(id + 1)); 
+            
+            // Range - Создает последовательность целых чисел в указанном диапазоне; в нашем случае от 1 до 100
+            // Select - Проецирует каждый элемент последовательности в IEnumerable<T>
+
+            //Маркер => называется лямбда - оператором.Он используется в лямбда - выражениях для отделения входных переменных 
+            //с левой стороны от тела лямбда-выражения с правой стороны.Лямбда - выражения — это встроенные выражения, 
+            //аналогичные анонимным методам, но более гибкие.
+        }
+
+        public static void NotMain()
+        {
+            var elements = Boo.Generate(100);
+
+            Console.WriteLine("Тип равен " + elements. GetType().ToString());
+        }
+    }
+}
+
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public string[] arrItemMenu_AB = new string[3] { "KAHAЛ   3AHЯT", "C/Ш ", "KAЧECTBO MEPИTЬ?"};
+        public string[] arrItemMenu_ABC = new string[2] { "xxx", "vvvvvvv" };
+
+        public Boo b1 ;
+
+        static string Union(string[] array)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                {
+                    sb.Append(array[i] + ",");
+                }
+            }
+            sb.Append(array[array.Length - 1]);
+
+            return sb.ToString();
+        }
+
+        static string Union_old(string[] array)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i + 1 == array.Length)
+                {
+                    sb.Append(array[i]);
+                }
+                else
+                {
+                    sb.Append(array[i] + ",");
+                }
+            }
+            return sb.ToString();
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            // Union = Union(arrItemMenu_ABC);
+
+            Console.WriteLine(Union(arrItemMenu_AB));
+
+            b1 = new Boo();
+        }
+    }
+}
